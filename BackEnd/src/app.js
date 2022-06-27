7896fa386f4e42ca4f808c73717726bb5cc92670
LuisBola12 <luibv1202@gmail.com>
2022-06-27 10:55:36 -0600

    Commit con implementacion del backend

---
+++ BackEnd/src/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import config from './config';
+import routes from './routes/pinPlay.routes';
+import morgan from 'morgan';
+import cors from "cors";
+
+const app = express();
+
+//Settings
+app.set('port',config.port);
+
+//Middlewares
+app.use(cors());
+app.use(morgan('combined'))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(routes);
+
+export default app;
