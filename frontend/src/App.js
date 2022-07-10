7b81acedcbd164b249f12e9ea87884f143ad2eb0
jarod-07 <72097024+jarod-07@users.noreply.github.com>
2022-07-10 14:33:50 -0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Login } from './Pages/login';
 import { Register } from './Pages/register';
-import { Unauthoraized } from './Pages/unauthoraized';
 import { Players } from './Pages/players';
 import { Classification } from'./Pages/classification';
 import PlayerProfile from './Pages/playerProfile';
@@ -17,7 +16,6 @@ function App() {
       <Routes>
         <Route path='/' element={<Tournaments />} />
         <Route path='login' element={<Login />} />
-        <Route path='no-autorizado' element={<Unauthoraized />} />
         <Route path='registrarse' element={<Register />} />
         <Route path='jugadores' element={<Players />} />
         <Route path='clasificacion' element={<Classification />} />
